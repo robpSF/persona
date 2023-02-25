b46c07e8a710de40fa3ae90d68d9223426de18d4
robpSF <robert@tstoryteller.com>
2023-02-25 23:27:03 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,19 +7,21 @@ openai.api_key = sl.secrets["key"]
 
 temp = sl.sidebar.slider("Craziness of ideas",0.0,1.0,0.5)
 
-originating_text = sl.text_area("enter starting text")
-scenario = sl.text_input("Enter prompt'")
+who_am_i = sl.text_area("who am I?")
+originating_text = sl.text_area("enter player question/response")
+scenario = sl.text_input("previous response")
 
 go = sl.button("do it!")
 
 if go:
     sl.sidebar.text("Drafting scenario")
 
-    sl.write(scenario+originating_text)
+    my_prompt = "You're acting as "+who_am_i+". "+"You previously responded with "+scenario+". Reply to:"+originating_text
+    sl.write(my_prompt)
 
     response = openai.Completion.create(
       model="text-davinci-003",
-      prompt=scenario+originating_text,
+      prompt=my_prompt,
       temperature=temp,
       max_tokens=1000,
       top_p=1.0,
